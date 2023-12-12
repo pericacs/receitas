793750213d4ae37f01abd6becd9880316ff8a34b
Carlos André Cardso dos Santos <pericacs@gmail.com>
2023-12-12 11:24:07 -0300

    Corrigindo arquivo pytest.ini

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,5 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 2, 'Um é igual a um'
+    def test_receita_home_url_is_correct(self):
+        url = reverse('receitas:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('receitas:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/receitas/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('receitas:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
+
+
+  
