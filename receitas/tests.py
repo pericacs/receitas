2ee9c142640bc55fc34283da914d6277a327119d
Carlos André Cardso dos Santos <pericacs@gmail.com>
2023-12-12 11:00:04 -0300

    arquivos de teste

--- receitas/tests.py
+++ receitas/tests.py
@@ -2,4 +2,4 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+        assert 1 == 2, 'Um é igual a um'
